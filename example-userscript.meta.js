db5fea99ac26499f11681733b571805a037c58cd
Simon Alling <alling.simon@gmail.com>
2018-11-11 16:13:56 +0100

    Example Userscript: @downloadURL and @updateURL

--- example-userscript.meta.js
+++ example-userscript.meta.js
@@ -1,7 +1,9 @@
 // ==UserScript==
-// @name     Example Userscript
-// @version  0.1.0
-// @match    *://example.com/*
-// @match    *://www.example.com/*
-// @run-at   document-start
+// @name         Example Userscript
+// @version      0.1.0
+// @match        *://example.com/*
+// @match        *://www.example.com/*
+// @run-at       document-start
+// @downloadURL  https://simonalling.github.io/userscripts/example-userscript.user.js
+// @updateURL    https://simonalling.github.io/userscripts/example-userscript.meta.js
 // ==/UserScript==
