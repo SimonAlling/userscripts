9b55265a88913b1d039c7efbb765608d263ba889
Simon Alling <alling.simon@gmail.com>
2018-11-11 16:02:10 +0100

    Add Example Userscript

---
+++ example-userscript.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     Example Userscript
+// @version  0.1.0
+// @match    *://example.com/*
+// @match    *://www.example.com/*
+// @run-at   document-start
+// ==/UserScript==
