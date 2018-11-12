2bb5c945dc9ac258f78b4cced44330a8e564bb5d
Simon Alling <alling.simon@gmail.com>
2018-11-12 08:28:38 +0100

    Userscripts

---
+++ better-azure-devops.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Better Azure DevOps
+// @version      0.1.0
+// @match        *://dev.azure.com/*
+// @run-at       document-start
+// @downloadURL  https://simonalling.github.io/userscripts/better-azure-devops.user.js
+// @updateURL    https://simonalling.github.io/userscripts/better-azure-devops.meta.js
+// ==/UserScript==
---
+++ telia-wifi-auto-login.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Telia Wi-Fi Auto-Login
+// @version      1.0.4
+// @match        *://*.teliawifi.telia.com/*
+// @run-at       document-start
+// @downloadURL  https://simonalling.github.io/userscripts/telia-wifi-auto-login.user.js
+// @updateURL    https://simonalling.github.io/userscripts/telia-wifi-auto-login.meta.js
+// ==/UserScript==
